00d31f520dce26d6ad242bfc2d8b33edd8552942
Jacob Unknown <53351513+jacobunknown@users.noreply.github.com>
2021-06-25 23:32:25 +0100

    Mainly website stuff

---
+++ footer.js
@@ -0,0 +1,3 @@
+document.write(`<footer>
+<p class="mid">Site created by <a href="https://jacobunknown.com">Jacob Unknown</a> using components from <a href="https://github.com/jacobunknown/css-library">css-library</a>.</p>
+</footer>`);
---
+++ header.js
@@ -0,0 +1,6 @@
+document.write(`<header>
+<nav class="navbar underlined purple">
+    <a href="index.html">Home</a>
+    <a href="docs/intro.html">Docs</a>
+</nav>
+</header>`);
