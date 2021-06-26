1bfa1a178ab33b4310af5783f52a6f6b1d45b74f
Jacob Unknown <53351513+jacobunknown@users.noreply.github.com>
2021-06-26 14:38:28 +0100

    Delete footer.js

--- footer.js
+++
@@ -1,3 +0,0 @@
-document.write(`<footer>
-<p class="mid">Site created by <a href="https://jacobunknown.com">Jacob Unknown</a> using components from <a href="https://github.com/jacobunknown/css-library">css-library</a>.</p>
-</footer>`);
